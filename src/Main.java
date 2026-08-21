40f331174c81b3b2dcc2c291e9d8f962fa08f712
pareekadityaa <adityaprk7@gmail.com>
2026-08-21 01:38:07 -0700

    Turn Java app into HTTP server

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-import java.util.Date;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from Java running in Kubernetes!";
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            OutputStream output = exchange.getResponseBody();
+            output.write(response.getBytes());
+            output.close();
+        });
+
+        server.start();
+
+        System.out.println("Java server running on port 8080");
     }
 }
